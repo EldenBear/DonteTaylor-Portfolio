dc24d3cc119f1198f0d5760dc019426bf8901033
EldenBear <dontet1995@gmail.com>
2023-07-08 11:41:07 -0400

    Rearranged app and made contact component for app

---
+++ src/components/pages/Contact.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <form id="contact-form">
+        <div>
+          <label for="Name">Name:</label>
+          <br />
+          <input type="text" name="Name" value="" />
+        </div>
+        <div>
+          <label for="email">Email address:</label>
+          <br />
+          <input type="email" name="email" value="" />
+        </div>
+        <div>
+          <label for="Message">Message:</label>
+          <br />
+          <textarea name="Message" rows="5"></textarea>
+          </div>
+          <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
