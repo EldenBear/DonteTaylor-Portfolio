72ee6c22ebef38c7e04892d8b792be0570059624
EldenBear <dontet1995@gmail.com>
2023-07-08 11:20:59 -0400

    Made About component for app

---
+++ components/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>
+         My name is LaDonte Taylor and I'm a full stack web developer currently studying at Rutgers Bootcamp. I plan on earning my certificate in August 2023 and
+         applying my newly learned skills into the field. I am known as an innovative problem solver with high attention to detail. My area of focus is front-end
+         web development using React. In my Portfolio section you'll see projects I've worked on solo as well as my group projects from Rutgers.
+      </p>
+    </div>
+  );
+}
