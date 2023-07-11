ddaa6f5158c6e04f822a9d997e5e50504beb7961
EldenBear <dontet1995@gmail.com>
2023-07-10 20:53:40 -0400

    Added third icon and link to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,9 +9,14 @@ export default function Footer() {
         <img src='../../github (1).png' alt='github' className='logo'>
           </img>
           </a>
+
           <a href='https://www.linkedin.com/in/ladonte-taylor/' className='footer-image-container'>
             <img src='../../linkedin (1).png' alt='linkedin' className='logo'>
+            </img>
+          </a>
 
+          <a href='https://stackoverflow.com/users/22206512/donte-taylor' className='footer-image-container'>
+            <img src='../../9567026_stack_overflow_development_icon.png' alt='Stack Overflow' className='logo'>
             </img>
           </a>
     </div>
