1120fbb7c2a295a0ebf26573e4b4af1b4d96dcb7
EldenBear <dontet1995@gmail.com>
2023-07-10 20:38:15 -0400

    Stylized Portfolio section

--- src/components/Project.js
+++ src/components/Project.js
@@ -2,12 +2,20 @@ import React from 'react';
 
 export default function Project(props) {
   return (
-    <div className='project'>
+    <>
+     <div className='project'>
       <div>{props.name}</div>
       <a href={props.link}>
-        <img src={props.src} href={props.link} alt='projectPhoto'>
+        <img src={props.src} href={props.link} alt='Project Screenshot'>
         </img> 
       </a>
     </div>
+    <a href={props.githubLink} alt='Deployed Github' className='githubDeploy'>
+    <img src='../../../github (2).png' alt='Projects Github Deployment' className='githubLinkIcon'>
+    </img>
+    </a>
+    <p className='projectDescription'>{props.description}</p>
+    </>
+   
   );
 }
